9028963554e2bf984ef2f916e0122e5e4d76a660
DallasThompson <duhtennis1@gmail.com>
2023-11-03 16:34:33 -0800

    configured workbox plugins, added css loaders and babel to webpack

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -17,12 +17,57 @@ module.exports = () => {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
-    plugins: [
+    plugins: [ 
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      title: 'Text Editor'
+    }),
+
+    new InjectManifest({
+      swSrc: './src-sw.js',
+      swDest: 'src-sw.js',
+          }),
+
+    new WebpackPwaManifest({
+      fingerprints: false,
+      inject: true,
+      name: 'Text Editor',
+      short_name: 'TextEdit',
+      description: 'Keep your notes organized!',
+      background_color: '#008080',
+      theme_color: '#ff7f50',
+      start_url: './',
+      publicPath: './',
+      icons: [
+        {
+          src: path.resolve('src/images/logo.png'),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join('assets', 'icons'),
+        },
+      ],
+    }),
+  ],
       
-    ],
+    
 
     module: {
       rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
         
       ],
     },
